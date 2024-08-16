672a0553a665dd790a64a799362ff614ac9ae500
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2024-08-16 15:48:41 +0200

    more changes again

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App'; // Adjust the path as necessary
-import './index.css'; // Import your global styles if you have any
+import './assets/index.css'; // Import your global styles if you have any
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
