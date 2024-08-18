c0e57c4408276a4eca1d8b8e9f9dc87d7060fd43
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2024-08-18 11:23:51 +0200

    fixed theme button colors

--- reactapp/src/utils/themeToggle.js
+++ reactapp/src/utils/themeToggle.js
@@ -1,26 +1,19 @@
 // Function to toggle between light and dark themes
 export function toggleTheme() {
-
     const body = document.body;
     const toggleButton = document.getElementById('theme-toggle');
     const isLightTheme = body.classList.toggle('light-theme');
 
-    // Toggle navbar class
-    const navbar = document.querySelector('.navbar');
+    // Update button text based on theme
     if (isLightTheme) {
         toggleButton.textContent = 'Dark Theme';
-        toggleButton.style.backgroundColor = '#1F2833';
-        toggleButton.style.color = '#E4EDFC';
     } else {
         toggleButton.textContent = 'Light Theme';
-        toggleButton.style.backgroundColor = '#E4EDFC';
-        toggleButton.style.color = '#1F2833';
     }
 
     // Store the user's preference in localStorage
     localStorage.setItem('theme', isLightTheme ? 'light' : 'dark');
 }
-
 
 // Apply the theme based on localStorage when the page loads
 window.onload = function () {
@@ -29,11 +22,7 @@ window.onload = function () {
     if (theme === 'light') {
         document.body.classList.add('light-theme');
         toggleButton.textContent = 'Dark Theme';
-        toggleButton.style.backgroundColor = '#1F2833'; // Dark background color
-        toggleButton.style.color = '#E4EDFC'; // Light text color
     } else {
         toggleButton.textContent = 'Light Theme';
-        toggleButton.style.backgroundColor = '#E4EDFC'; // Light background color
-        toggleButton.style.color = '#1F2833'; // Dark text color
     }
 };
