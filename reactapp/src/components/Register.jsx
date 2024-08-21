88f8f5f7a1d649ba5a5bca41104e12132815fda9
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2024-08-21 09:15:34 +0200

    fixed api url in Login.jsx and Register.jsx

--- reactapp/src/components/Register.jsx
+++ reactapp/src/components/Register.jsx
@@ -8,7 +8,7 @@ const Register = () => {
 
   const handleRegister = async () => {
     try {
-      const response = await fetch('http://localhost:3000/api/register', {
+      const response = await fetch('http://localhost:3000/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
