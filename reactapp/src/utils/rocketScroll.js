503e8ff974f16ecce2500d885ff84df4698d8390
Jimmy Hansen <40581118+Ymmuu@users.noreply.github.com>
2024-08-21 12:10:28 +0200

    cant change the starting position

--- reactapp/src/utils/rocketScroll.js
+++ reactapp/src/utils/rocketScroll.js
@@ -14,28 +14,22 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("scroll", handleScroll);
 
     rocket.addEventListener('click', function() {
-        // Scroll to the top of the page instantly
         window.scrollTo({ top: 0, behavior: 'smooth' });
         
-        const totalFrames = 21; // Total number of rocket images
+        const totalFrames = 21;
         let currentFrame = 1;
 
-        // Function to animate the rocket
         function animateRocket() {
-            // Ensure the image path is correct
             rocket.src = `/images/Rocket/Rocket${currentFrame}.png`;
             currentFrame++;
 
-            // If the last frame is reached, stop the animation
             if (currentFrame > totalFrames) {
                 clearInterval(animationInterval);
-                // Apply the rotation and move rocket to the top of the page
                 rocket.style.transform = 'rotate(360deg)';
-                rocket.style.top = '0px';  // Move to the top of the page
+                rocket.style.top = '0px';
             }
         }
 
-        // Start the animation
-        const animationInterval = setInterval(animateRocket, 50); // Adjust the interval to control speed
+        const animationInterval = setInterval(animateRocket, 50);
     });
 });
